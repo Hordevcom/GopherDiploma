f5b6ddfe4fd705ff0c3170d0a53991420e7597cf
Viel <vielavernus@gmail.com>
2025-04-01 10:18:56 +0300

    add logger and router and server

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/Hordevcom/GopherDiploma/internal/handlers/routes"
+	"github.com/Hordevcom/GopherDiploma/internal/middleware/logging"
+)
+
+func main() {
+
+	logger := logging.NewLogger()
+	router := routes.NewRouter()
+
+	server := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: router,
+	}
+
+	logger.Infow("Start server", "addr: ", server.Addr)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalw("create server err: ", err)
+	}
+}
---
+++ internal/handlers/routes/routes.go
@@ -0,0 +1,9 @@
+package routes
+
+import "github.com/go-chi/chi/v5"
+
+func NewRouter() *chi.Mux {
+	router := chi.NewRouter()
+
+	return router
+}
