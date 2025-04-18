dec09a13b9fdcd2fa70b0cd141d634ced8cc3987
Viel <vielavernus@gmail.com>
2025-04-18 15:29:37 +0300

    add more logs

--- internal/service/userBalanceWithdrawn.go
+++ internal/service/userBalanceWithdrawn.go
@@ -11,6 +11,7 @@ import (
 func BalanceWithdrawn(ctx context.Context, currentBalance float32, withdrawn models.UserWithdrawal, db storage.PGDB, user string) error {
 	fmt.Println("Sum is: ", withdrawn.Sum)
 	finalSum := currentBalance - withdrawn.Sum
+	fmt.Println("finalSum is: ", withdrawn.Sum)
 	err := db.UpdateUserBalance(ctx, user, finalSum, withdrawn.Sum)
 
 	if err != nil {
